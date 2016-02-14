7f79b3c68dfca5fe3eccf9c8d7b95c96fe8fb115
RigFox <rigfox@gmail.com>
2016-02-14 15:07:42 +0500

    Initial commit

---
+++ js/base.js
@@ -0,0 +1,131 @@
+var selectDate;
+
+function timeToDays(time) {
+    return time / 1000 / 60 / 60 / 24;
+}
+
+function getWeekNumber(date) {
+    var weekNumber;
+    var yearStart;
+    var ts;
+
+    ts = new Date(date);
+
+    ts.setHours(0, 0, 0);
+    ts.setDate(ts.getDate() + 4 - (ts.getDay() || 7));
+
+    yearStart = new Date(ts.getFullYear(), 0, 1);
+
+    weekNumber = Math.floor((timeToDays(ts - yearStart) + 1) / 7);
+
+    return weekNumber + 1;
+}
+
+function show(date) {
+    var weekOfYear = getWeekNumber(date) + 17;
+    var dayOfWeek = date.getDay() - 1;
+
+    var prepSchedule = [];
+
+    var el = schedule.schedule[dayOfWeek];
+
+    el.forEach(function (lesson) {
+        if ((lesson.startWeek <= weekOfYear) && (lesson.endWeek >= weekOfYear)) {
+            prepSchedule[lesson.num - 1] = lesson;
+        }
+    });
+
+    $('#content').find('>.lesson').each(function (index, el) {
+        var time = $(el).find('td')[0];
+        var title = $(el).find('td')[1];
+        var week = $(el).find('td')[2];
+
+        var titleText;
+        var weekText;
+
+        var scheduleObj = prepSchedule[index];
+
+        if (scheduleObj != undefined) {
+            var subject = schedule.subjects[scheduleObj.subject_id];
+
+            titleText = subject.name;
+            titleText += "<br/>" + subject.teacher;
+
+            weekText = "("+scheduleObj.startWeek+"-"+scheduleObj.endWeek+")";
+            weekText += "<br/>"+subject.classroom;
+        } else {
+            titleText = '';
+            weekText = '';
+        }
+
+        var timeText;
+
+        switch (index) {
+            case 0:
+                timeText = "8:00-<br/>9:35";
+                break;
+            case 1:
+                timeText = "9:45-<br/>11:20";
+                break;
+            case 2:
+                timeText = "11:30-<br/>13:05";
+                break;
+            case 3:
+                timeText = "13:30-<br/>15:05";
+                break;
+            case 4:
+                timeText = "15:15-<br/>16:50";
+                break;
+        }
+
+        time.innerHTML = timeText;
+        title.innerHTML = titleText;
+        week.innerHTML = weekText;
+    });
+
+    var dayOfWeekText;
+
+    switch (dayOfWeek) {
+        case 0:
+            dayOfWeekText = "Понедельник"; break;
+        case 1:
+            dayOfWeekText = "Вторник"; break;
+        case 2:
+            dayOfWeekText = "Среда"; break;
+        case 3:
+            dayOfWeekText = "Четверг"; break;
+        case 4:
+            dayOfWeekText = "Пятница"; break;
+        case 5:
+            dayOfWeekText = "Суббота"; break;
+    }
+
+    $('#dayOfWeek').text(dayOfWeekText);
+    $('#weekOfYear').text(weekOfYear+" неделя");
+}
+
+function today() {
+    selectDate = new Date();
+    if (selectDate.getDay() == 0) { //Sunday
+        selectDate = new Date(selectDate.getFullYear(), selectDate.getMonth(), selectDate.getDate()+1);
+    }
+    show(selectDate);
+}
+
+function backDay() {
+    selectDate = new Date(selectDate.getFullYear(), selectDate.getMonth(), selectDate.getDate()-1);
+    if (selectDate.getDay() == 0) { //Sunday
+        selectDate = new Date(selectDate.getFullYear(), selectDate.getMonth(), selectDate.getDate()-1);
+    }
+    show(selectDate);
+}
+
+function nextDay() {
+    selectDate = new Date(selectDate.getFullYear(), selectDate.getMonth(), selectDate.getDate()+1);
+    if (selectDate.getDay() == 0) { //Sunday
+        selectDate = new Date(selectDate.getFullYear(), selectDate.getMonth(), selectDate.getDate()+1);
+    }
+    show(selectDate);
+}
+
+today();
---
+++ schedule.js
@@ -0,0 +1,57 @@
+var schedule = {
+    "subjects": [
+      {"name": "Дифференциальные уравнения", "teacher": "доц. Муртазина С. А.", "classroom": "ауд. 304"},
+      {"name": "Комплексный анализ", "teacher": "доц. Музафаров С.М.", "classroom": "ауд. 304"},
+      {"name": "Операционные системы", "teacher": "ст. пр. Хисаметдинов Ф. З.", "classroom": "ауд. 304"},
+      {"name": "Языки и методы программирования", "teacher": "доц. Вышинский А. А.", "classroom": "ауд. 301"},
+      {"name": "Практикум на ЭВМ", "teacher": "ст. пр. Якшибаева Д. А.", "classroom": "ауд. 301"},
+      {"name": "Элективные курсы по физической культуре", "teacher": "проф. Полько Г. М.", "classroom": ""},
+      {"name": "Теоретическая механика", "teacher": "доц. Кунсбаева Г. А.", "classroom": "ауд. 304"},
+      {"name": "Иностранный язык", "teacher": "доц. Хасанова Г. Н.", "classroom": "ауд. 304"},
+      {"name": "Компьютерная графика", "teacher": "ст. пр. Якшибаева Д. А.", "classroom": "ауд. 301"},
+      {"name": "Языки и методы программирования", "teacher": "доц. Вышинский А. А.", "classroom": "ауд. 304"}
+],
+    "schedule": [
+    [
+        {"num": 1, "subject_id": 0, "startWeek": 25, "endWeek": 38},
+        {"num": 2, "subject_id": 1, "startWeek": 25, "endWeek": 38},
+        {"num": 3, "subject_id": 1, "startWeek": 25, "endWeek": 38},
+        {"num": 4, "subject_id": 2, "startWeek": 25, "endWeek": 28},
+        {"num": 4, "subject_id": 3, "startWeek": 29, "endWeek": 38}
+    ],
+    [
+        {"num": 1, "subject_id": 0, "startWeek": 25, "endWeek": 38},
+        {"num": 2, "subject_id": 0, "startWeek": 25, "endWeek": 38},
+        {"num": 3, "subject_id": 3, "startWeek": 25, "endWeek": 29},
+        {"num": 3, "subject_id": 4, "startWeek": 30, "endWeek": 38},
+        {"num": 4, "subject_id": 5, "startWeek": 25, "endWeek": 38}
+    ],
+    [
+        {"num": 1, "subject_id": 5, "startWeek": 25, "endWeek": 38},
+        {"num": 2, "subject_id": 6, "startWeek": 25, "endWeek": 38},
+        {"num": 3, "subject_id": 4, "startWeek": 25, "endWeek": 38},
+        {"num": 4, "subject_id": 1, "startWeek": 25, "endWeek": 38}
+    ],
+    [
+        {"num": 1, "subject_id": 6, "startWeek": 25, "endWeek": 38},
+        {"num": 2, "subject_id": 7, "startWeek": 25, "endWeek": 38},
+        {"num": 3, "subject_id": 4, "startWeek": 25, "endWeek": 38},
+        {"num": 4, "subject_id": 8, "startWeek": 25, "endWeek": 38}
+    ],
+    [
+        {"num": 1, "subject_id": 4, "startWeek": 25, "endWeek": 38},
+        {"num": 2, "subject_id": 6, "startWeek": 25, "endWeek": 32},
+        {"num": 2, "subject_id": 8, "startWeek": 33, "endWeek": 36},
+        {"num": 3, "subject_id": 1, "startWeek": 25, "endWeek": 38},
+        {"num": 4, "subject_id": 2, "startWeek": 25, "endWeek": 38},
+        {"num": 5, "subject_id": 5, "startWeek": 32, "endWeek": 38}
+    ],
+    [
+        {"num": 1, "subject_id": 7, "startWeek": 25, "endWeek": 27},
+        {"num": 1, "subject_id": 9, "startWeek": 28, "endWeek": 29},
+        {"num": 1, "subject_id": 1, "startWeek": 30, "endWeek": 30},
+        {"num": 2, "subject_id": 9, "startWeek": 25, "endWeek": 29},
+        {"num": 2, "subject_id": 1, "startWeek": 30, "endWeek": 30}
+    ]
+]
+}
